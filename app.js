67704cccebd1710c3bf783bc45aaf06b093d1b46
MiniChii <f.p.chii@gmail.coom>
2019-07-01 00:25:26 -0400

    Create app.js
    + lo básico para que funcione la aplicación
    + y el listar productos

---
+++ app.js
@@ -0,0 +1,55 @@
+//Requires
+var express = require("express");
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+//para encriptar la contrseña
+var bcrypt = require('bcrypt');
+//para ocupar JWT (JsonWebToken)
+var jwt=require('jsonwebtoken');
+//para el cors
+var cors=require('cors');
+
+//Inicializar variable
+var app=express();
+app.use(cors());
+app.use(bodyParser.json());//para acceder a los paramettros post   
+app.use(bodyParser.urlencoded({extended:true})); //para poder adjuntar archivos
+app.use(fileUpload());
+
+//CORS Middleware: para que no lance errores de seguridad
+app.use(function(req,res,next){
+    //enabling cors
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+    
+
+//conf conexion
+const mc = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database: 'canito',
+});
+//conectar
+mc.connect();
+
+
+//Escuchar peticiones
+app.listen(3005,()=>{
+    console.log('Express server - puerto 3005 online');
+    
+});
+
+//productos se ponen los más espécificos primero
+app.get('/productos', function(req,res){
+    mc.query('SELECT * FROM producto', function(error,  results, fields ){
+        if(error) throw error;
+        return res.send({error: false, data: results, message:'Lista de productos.'})
+    })
+})
