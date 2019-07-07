7a44cff35a5393f6e31511870258abf89f3f5cff
MChavezSaa <matroxmega@gmail.com>
2019-07-07 17:17:54 -0400

    ADDED pedido.js

---
+++ pedido.js
@@ -0,0 +1,51 @@
+const mysql = require('mysql');
+//conf conexion
+const mc = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',
+    database: 'canito',
+});
+//conectar
+mc.connect();
+/*
+id int(11) primary key auto_increment,
+                              modo_entrega text NOT NULL,
+                              fecha_inicio date not null,
+                              fecha_entrega date  NOT NULL,
+                              fecha_pago date NOT NULL,
+                              valor_total int(11)  NOT NULL,
+                              metodo_pago text not null,
+                              direccion varchar(200) NOT NULL,
+                              estado varchar(150) NOT NULL */ 
+/*Crear pedido */
+exports.crearPedido=function(req,res){
+    let datosPedido = {  
+        modo_entrega: req.body.modo_entrega,
+        fecha_inicio: req.body.fecha_inicio,
+        fecha_entrega: req.body.fecha_entrega,
+        fecha_pago: req.body.fecha_pago,
+        valor_total: req.body.valor_total,
+        metodo_pago: req.body.metodo_pago,
+        direccion: req.body.direccion,
+        estado: req.body.estado
+    };
+
+    if(mc){
+        mc.query("INSERT INTO pedido SET ?",datosPedido,function (error,result){
+            
+            if(error){
+                res.status(500).json({
+                    Mensaje:"Error",
+                    error: error
+            })
+            }
+            else{
+                res.status(201).json({
+                    mensaje:"Pedido Insertado",
+                    datos: result
+                })
+            }
+        });
+    }
+};
