8d867f873a6d11695f77098ef9d99b9824ddcd7f
MChavezSaa <matroxmega@gmail.com>
2019-07-10 10:41:13 -0400

    se creo empleado

---
+++ empleado.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql');
+//conf conexion
+const mc = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'canito',
+});
+//conectar
+mc.connect();
+
+exports.crearEmpleado = function (req, res) {
+    let datosEmpleado = {
+       user: req.body.user,
+       password: req.body.password,
+       rol: req.body.rol
+
+    };
+
+    if (mc) {
+        console.log(datosEmpleado);
+        mc.query("INSERT INTO empleado SET ?", datosEmpleado, function (error, result) {
+
+            if (error) {
+                res.status(500).json({
+                    Mensaje: "Error",
+                    error: error
+                })
+            }
+            else {
+                res.status(201).json({
+                    mensaje: "Empleado Insertado",
+                    datos: result
+                })
+            }
+        });
+    }
+};
