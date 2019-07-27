75bb4b3ea586d21acefda2dc81d2eae16fedf5c0
MChavezSaa <matroxmega@gmail.com>
2019-07-27 16:49:23 -0400

    sql previo a cambios

--- canito.sql
+++ canito.sql
@@ -2,9 +2,9 @@
 -- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 03-07-2019 a las 23:39:02
--- Versión del servidor: 10.3.15-MariaDB
+-- Servidor: localhost
+-- Tiempo de generación: 27-07-2019 a las 20:48:27
+-- Versión del servidor: 10.3.12-MariaDB
 -- Versión de PHP: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -19,26 +19,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `canito`
+-- Base de datos: `matias.chavez1501`
 --
-CREATE DATABASE IF NOT EXISTS `canito` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `canito`;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cliente`
---
-
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL,
-  `email` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `contraseña` varchar(300) COLLATE utf8_spanish_ci NOT NULL,
-  `rut` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
-  `nombres` varchar(70) COLLATE utf8_spanish_ci NOT NULL,
-  `ap_paterno` varchar(40) COLLATE utf8_spanish_ci NOT NULL,
-  `ap_materno` varchar(40) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -52,7 +34,74 @@ CREATE TABLE `detalle_pedido` (
   `id_pedido` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`id`, `user`, `password`, `rol`) VALUES
+(2, 'Admin', '$2b$10$2PS3XjB41ybdUSQonm8RTOjE9wxG3iYM8SJGqkHxIaUO3QZiYPUa.', 'administrador'),
+(12, 'Felipe Vera', '$2b$10$LvOK0xYk3LNY5EsmbiyceemxYOZhzEfeaaKF25xBAOe6amcnNIB6G', 'vendedor'),
+(13, 'francisca', '$2b$10$b1oXcZjwAeImkNCkInA.nuFceCekJRqMVdLYO5nNojhVU.77w.Cw.', 'vendedor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `user` text NOT NULL,
+  `password` text NOT NULL,
+  `rol` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `login`
+--
+
+INSERT INTO `login` (`id`, `user`, `password`, `rol`) VALUES
+(5, 'admin', '123456', 'admin'),
+(6, 'maty@gmail.com', '12345', 'vendedor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opinion`
+--
+
+CREATE TABLE `opinion` (
+  `id` int(11) NOT NULL,
+  `nombreUsuario` varchar(150) NOT NULL,
+  `comentario` varchar(2500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `opinion`
+--
+
+INSERT INTO `opinion` (`id`, `nombreUsuario`, `comentario`) VALUES
+(2, 'alvaro castillo', 'Muy ricas sus tortas'),
+(3, 'Constanza', 'Muy recomendable :D!'),
+(4, 'Matias', 'Recomendable al 100%'),
+(5, 'Clemente', 'Me encanta su panaderia'),
+(6, 'Rodrigo', 'Me encantan las empanadas de su local y muy buena la atencion'),
+(7, 'Felipe Vilchez', 'Muy buena panaderia!');
 
 -- --------------------------------------------------------
 
@@ -62,15 +111,22 @@ CREATE TABLE `detalle_pedido` (
 
 CREATE TABLE `pedido` (
   `id` int(11) NOT NULL,
-  `modo_entrega` enum('retiro local','despacho domicilio') COLLATE utf8_spanish_ci NOT NULL DEFAULT 'retiro local',
-  `fecha_inicio` date NOT NULL DEFAULT current_timestamp(),
+  `modo_entrega` enum('retiro local','despacho domicilio') NOT NULL DEFAULT 'retiro local',
+  `fecha_inicio` date NOT NULL,
   `fecha_entrega` date NOT NULL,
   `fecha_pago` date NOT NULL,
   `valor_total` int(11) NOT NULL,
-  `metodo_pago` enum('débito','crédito','efectivo') COLLATE utf8_spanish_ci NOT NULL,
-  `direccion` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
-  `estado` enum('cancelado por cliente','procesando','aprobado','preparando','listo para entrega','entregado') COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `metodo_pago` enum('débito','crédito','efectivo') NOT NULL,
+  `direccion` varchar(200) NOT NULL,
+  `estado` enum('cancelado por cliente','procesando','aprobado','preparando','listo para entrega','entregado') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `pedido`
+--
+
+INSERT INTO `pedido` (`id`, `modo_entrega`, `fecha_inicio`, `fecha_entrega`, `fecha_pago`, `valor_total`, `metodo_pago`, `direccion`, `estado`) VALUES
+(1, 'retiro local', '2019-07-07', '2019-07-07', '2019-07-07', 111111111, 'débito', 'lalal', 'preparando');
 
 -- --------------------------------------------------------
 
@@ -80,33 +136,39 @@ CREATE TABLE `pedido` (
 
 CREATE TABLE `producto` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(70) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(70) NOT NULL,
   `precio` int(11) NOT NULL,
-  `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
-  `categoria` enum('empanada','torta','pan') COLLATE utf8_spanish_ci NOT NULL,
-  `imagen` text COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text NOT NULL,
+  `categoria` enum('Empanadas','Tortas','Panaderia') NOT NULL,
+  `imagen` text NOT NULL,
   `cantidad_personas` int(11) DEFAULT NULL,
   `tiempo_produccion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`id`, `nombre`, `precio`, `descripcion`, `categoria`, `imagen`, `cantidad_personas`, `tiempo_produccion`) VALUES
-(1, 'Mi hojas', 15000, 'Torta de hoja, rellena con manjar y trozos de nueces', 'torta', 'imagen de tortita :3', 20, 0),
-(2, 'Mi hojas', 15000, 'Torta de hoja, rellena con manjar y trozos de nueces', 'torta', 'imagen de tortita :3', 15, 0);
+(3, 'Bocado Príncipe', 3490, 'Masas de hoja con nuez, manjar y mermelada de damasco cubierta con merengue.', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta1.jpg', 10, 1),
+(4, 'Brasileira', 5000, 'Panqueque blanco con pure? de lu?cuma, manjar y cubierta con ralladura de chocolate.', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta2.jpg', 6, 1),
+(5, 'Chocolate', 6000, 'Panqueque chocolate con crema de chocolate cubierta con chocolate.', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta3.jpg', 6, 1),
+(6, 'Chocolate Frambuesa\r\n', 4000, 'Panqueque chocolate con crema de chocolate y mermelada de frambuesa cubierta con chocolate.', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta4.jpg', 5, 1),
+(7, 'Chocolate Manjar', 4500, 'Panqueque chocolate con crema de chocolate y manjar cubierta con chocolate.', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta5.jpg', 4, 1),
+(8, '\r\nFlorencia', 3400, 'Bizcocho blanco, bizcocho chocolate, masas de hojas, disco de merengue, manjar y mermelada de frutilla cubierta de crema chantilli?.', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta6.jpg', 4, 1),
+(9, '\r\nMil Hoja\r\n', 4100, 'Hoja de alfajor con manjar.\r\n\r\n', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta8.jpg', 4, 1),
+(10, 'Mil Hoja Frambuesa', 4300, 'Hoja de alfajor con manjar y mermelada de frambuesa.', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta9.jpg', 7, 1),
+(11, '\r\nCeliaco\r\n', 5000, 'Bizcocho Chocolate (hecho de harina de almendra) con crema chocolate, mermelada frambuesa cubierta de crema de chocolate y placas de chocolate.', 'Tortas', 'http://www.pastelerialaabuelita.cl/img/torta15.jpg', 10, 1),
+(12, 'Empanada Pino Grande 24cm', 1300, 'Horneada y lista para servir. Incluye 1 unidad.', 'Empanadas', 'https://res.cloudinary.com/admsys-storage/image/fetch/c_fill,f_auto,q_auto:best,w_auto/https://elpalaciodelasempanadas.cl/site/resources/uploads/productos/normal/7b05557f7e5f9c09d522636354e32b58.jpg', 1, 1),
+(13, 'Empanadas Queso Medialuna Grande', 1600, 'Para freír. Incluye 5 unidades.', 'Empanadas', 'https://res.cloudinary.com/admsys-storage/image/fetch/c_fill,f_auto,q_auto:best,w_auto/https://elpalaciodelasempanadas.cl/site/resources/uploads/productos/normal/0a7b487f4ba6fffaf5f758833d7bc250.jpg', 1, 1),
+(14, 'Sopaipilla Coctel', 1600, 'para Freir.incluye 40 unidades', 'Empanadas', 'https://res.cloudinary.com/admsys-storage/image/fetch/c_fill,f_auto,q_auto:best,w_auto/https://elpalaciodelasempanadas.cl/site/resources/uploads/productos/normal/c60fced93dff4223f6479655bb586ce1.jpg', 1, 1),
+(15, 'Pan Ciabatta Rústico', 500, 'Pan Rustico de calidad', 'Panaderia', 'https://jumbo.vteximg.com.br/arquivos/ids/294068-750-750/Principal-1304.jpg?v=636869721493230000', 1, 1),
+(16, 'Croissant artesano', 359, 'destaca por su crujiente hojaldre y su esponjoso interior. ¡Un clásico imprescindible! Con un formato ideal para desayunos y meriendas.', 'Panaderia', 'https://jumbo.vteximg.com.br/arquivos/ids/296384-750-750/295669-01.png?v=636886002290870000', 1, 1),
+(17, 'Pan Coliza Peruana', 240, 'pan algo crocante y de sabor un poco dulce', 'Panaderia', 'https://jumbo.vteximg.com.br/arquivos/ids/294103-750-750/Principal-134637.jpg?v=636869721677230000', 1, 1);
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indices de la tabla `detalle_pedido`
@@ -116,6 +178,24 @@ ALTER TABLE `detalle_pedido`
   ADD UNIQUE KEY `id_producto` (`id_producto`),
   ADD UNIQUE KEY `id_pedido` (`id_pedido`),
   ADD UNIQUE KEY `id_cliente` (`id_cliente`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `opinion`
+--
+ALTER TABLE `opinion`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `pedido`
@@ -134,28 +214,40 @@ ALTER TABLE `producto`
 --
 
 --
--- AUTO_INCREMENT de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `detalle_pedido`
 --
 ALTER TABLE `detalle_pedido`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `opinion`
+--
+ALTER TABLE `opinion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT de la tabla `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Restricciones para tablas volcadas
